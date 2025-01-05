9896523a5040c42981d1093604adccaec85ab4cb
T_Mori <morit0301@gmail.com>
2025-01-05 17:09:01 +0900

    Implementing a function to display book data - Introducing TanStack Query and JSON Server - Implementing an API client

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import BoardColumn from "./components/BoardColumn";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { COLUMNS } from "./constants";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // デフォルトでコンポーネントにフォーカスが当たるとフェッチするのを無効化
+      refetchOnWindowFocus: false,
+      // 取得したデータが「古い」とみなされるまでの時間。デフォルトで 0。
+      staleTime: Infinity,
+    },
+  },
+});
+
 const App = () => {
   return (
-    <div className="min-h-screen pt-16 pb-8 space-y-8 px-16 bg-gray-100">
-      <Header />
-      <div className="flex gap-6 overflow-x-auto pb-4">
-        {COLUMNS.map((column) => (
-          <BoardColumn key={column.status} status={column.status} />
-        ))}
+    <QueryClientProvider client={queryClient}>
+      <div className="min-h-screen pt-16 pb-8 space-y-8 px-16 bg-gray-100">
+        <Header />
+        <div className="flex gap-6 overflow-x-auto pb-4">
+          {COLUMNS.map((column) => (
+            <BoardColumn key={column.status} status={column.status} />
+          ))}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </QueryClientProvider>
   );
 };
 
--- src/components/BoardColumn.tsx
+++ src/components/BoardColumn.tsx
@@ -1,5 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { COLUMNS } from "../constants";
 import { BookStatus } from "../types";
+import { fetchBooks } from "@/utils/api";
 
 type BoardColumnProps = {
   status: BookStatus;
@@ -10,6 +12,29 @@ const BoardColumn = (props: BoardColumnProps) => {
   const label = columnData?.label;
   const title = columnData?.title;
 
+  // 追加
+  const {
+    data: books = [],
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["books"],
+    queryFn: fetchBooks,
+    select: (data) => data.filter((book) => book.status === props.status),
+  });
+
+  console.log("データ取得"); // デバッグ用
+
+  if (isLoading) {
+    return (
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-emerald-500 border-t-transparent" />
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <section className="flex-1 min-w-[300px] bg-white border-gray-300 border p-6 rounded-lg">
       <div className="border mb-6 border-gray-300 text-3xl h-16 w-16 rounded-full flex justify-center items-center">
@@ -19,8 +44,10 @@ const BoardColumn = (props: BoardColumnProps) => {
         <h2 className="font-bold text-xl">{title}</h2>
       </div>
       <div className="space-y-2">
-        <p>A</p>
-        <p>B</p>
+        {/* 書き換え */}
+        {books.map((book) => (
+          <p key={book.id}>📚 {book.title}</p>
+        ))}
       </div>
     </section>
   );
